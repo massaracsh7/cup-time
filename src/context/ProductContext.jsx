8be9a81a70f2a135904e2f94f9f6b0032090e477
massaracsh7 <fatum7@gmail.com>
2024-08-02 20:07:42 +0300

    feat: add connect with api

---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { API_URL } from '../const';
+
+const ProductContext = createContext();
+
+export const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    if (category) {
+      fetch(`${API_URL}/api/products/${category}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          return response.json()
+        })
+        .then((data) => setProducts(data))
+        .catch((error) => console.error(`Error fetching products: ${error.message}`));
+    }
+  }, [category]);
+
+  return (
+    <ProductContext.Provider value={{ products, setCategory }}>
+      {children}
+    </ProductContext.Provider>
+  )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useProducts = () => useContext(ProductContext);
--- src/modules/Product.jsx
+++ src/modules/Product.jsx
@@ -1,9 +1,10 @@
+import { API_URL } from "../const"
 export const Product = ({data}) => {
 
   return (
     <li className="products__item">
       <article className="product">
-        <img src={data.image} alt={data.title} className="product__image" />
+        <img src={`${API_URL}${data.img}`} alt={data.title} className="product__image" />
         <div className="product__content">
           <h3 className="product__title">{data.title}</h3>
           <p className="product__price">{data.price} ₽</p></div>
