5badf939c177d48c0c18204269ff738041c2038d
massaracsh7 <fatum7@gmail.com>
2024-08-02 20:30:04 +0300

    feat: add modal

--- src/modules/Product.jsx
+++ src/modules/Product.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react"
 import { API_URL } from "../const"
-export const Product = ({data}) => {
+import { ProductModal } from "./ProductModal";
+export const Product = ({ data }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = (event) => {
+    event.preventDefault();
+    setModalIsOpen(true);
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  }
 
   return (
     <li className="products__item">
-      <article className="product">
-        <img src={`${API_URL}${data.img}`} alt={data.title} className="product__image" />
-        <div className="product__content">
-          <h3 className="product__title">{data.title}</h3>
-          <p className="product__price">{data.price} ₽</p></div>
-      </article>
+      <a className="product__link" href="#" onClick={openModal} aria-label={`Модальное окно для ${data.title}`}>
+        <article className="product">
+          <img src={`${API_URL}${data.img}`} alt={data.title} className="product__image" />
+          <div className="product__content">
+            <h3 className="product__title">{data.title}</h3>
+            <p className="product__price">{data.price} ₽</p></div>
+        </article></a>
+      <ProductModal isOpen={modalIsOpen} onRequestClose={closeModal} data={data} />
     </li>
   )
 }
