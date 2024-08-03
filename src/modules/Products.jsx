df47ea334be8e42138f046d231d965fde311e4bf
massaracsh7 <fatum7@gmail.com>
2024-08-03 19:34:39 +0300

    feat: add loader

--- src/modules/Products.jsx
+++ src/modules/Products.jsx
@@ -2,8 +2,7 @@ import { useEffect } from "react";
 import { useProducts } from "../context/ProductContext";
 import { Product } from "./Product"
 import { useSearchParams } from "react-router-dom";
-
-
+import { SkeletonLoader } from "./SkeletonLoader";
 
 export const Products = () => {
   const [searchParams] = useSearchParams();
@@ -21,17 +20,17 @@ export const Products = () => {
     cezves: "Турки",
     other: "Прочее"
   };
-
-
   return (
     <section className="products">
       <div className="container">
         <h2 className="products__title"> {category ? `${categoryTranslations[category] || category}` : 'Чай'}
         </h2>
         <ul className="products__list">
-          {products.map((item) => (
-            <Product key={item.id} data={item} />
-          ))}
+          { products.length ? ( products.map((item) => 
+            <Product key={item.id} data={item} />)
+            ) : ( 
+            <SkeletonLoader/>
+            )}
         </ul>
       </div>
     </section>
